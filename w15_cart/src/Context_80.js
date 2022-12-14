1f3e73bf8ee95b74342cfa8e4b7f64b2a9ad7fa6
justin40715 <justin40715@gmail.com>
2022-12-14 21:20:08 +0800

    W15-P4: provide increase and get total function in global context

--- w15_cart/src/Context_80.js
+++ w15_cart/src/Context_80.js
@@ -14,11 +14,28 @@ const initialState = {
 
 const AppProvider_80 = ({children}) => {
   const [state, dispatch] = useReducer(Reducer_80, initialState);
+
+  useEffect(() => {
+    dispatch({ type: 'GET_TOTAL'});
+  }, [state.cart]);
+
   const clearCart = () => {
     dispatch({ type: 'CLEAR_CART' });
   }
 
-  return <AppContext_80.Provider value={{...state,clearCart}}>
+  const increase = (id) => {
+    dispatch({ type: 'INCREASE', payload: id})
+  }
+
+  const decrease = (id) => {
+    dispatch({ type: 'DECREASE', payload: id})
+  }
+
+  const removeCart = (id) => {
+    dispatch({ type: 'REMOVE_CART', payload: id});
+  }
+
+  return <AppContext_80.Provider value={{...state,clearCart,increase,decrease,removeCart}}>
       {children}
   </AppContext_80.Provider>
 };
